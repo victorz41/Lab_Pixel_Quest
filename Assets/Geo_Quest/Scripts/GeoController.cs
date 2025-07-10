362382c171f2fd1099ef1d66b63178be8df20495
victorz41 <victorz41343@gmail.com>
2025-07-10 12:13:45 -0400

    hit move box

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
-    int var1 = 1;
- 
+    private Rigidbody2D rb;
+    public int speed = 3;
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
+        float xSpeed = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xSpeed * speed, rb.velocity.y);
+    }
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
+
+
+
+
+
+
+        //rb.velocity = new Vector2(-1, rb.velocity.y);
         // print(var1);
         //var1++;
         //transform.position += new Vector3(0.0005f, 0, 0);
@@ -24,25 +42,25 @@ public class GeoController : MonoBehaviour
         //print("67");
         //else
         //print("gurt");
-        if (Input.GetKeyDown(KeyCode.W)  || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Mouse0)) 
-        { 
-        transform.position += new Vector3(0, 1, 0);
-        }
+        //if (Input.GetKeyDown(KeyCode.W)  || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Mouse0)) 
+        //{ 
+        //transform.position += new Vector3(0, 1, 0);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-        }
+        //if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    transform.position += new Vector3(-1, 0, 0);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.position += new Vector3(0, -1, 0);
-        }
+        //if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    transform.position += new Vector3(0, -1, 0);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(1, 0, 0);
-        }
+        //if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+         //   transform.position += new Vector3(1, 0, 0);
+        //}
 
-    }
+    
 }
